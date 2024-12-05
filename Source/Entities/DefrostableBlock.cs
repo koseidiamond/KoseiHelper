fbb2162a767cebc0d68dafe86344579e9c4a046b
Kosei <109972200+koseidiamond@users.noreply.github.com>
2024-12-05 18:21:50 +0100

    defrostable core mode fix

--- Source/Entities/DefrostableBlock.cs
+++ Source/Entities/DefrostableBlock.cs
@@ -19,6 +19,7 @@ public class DefrostableBlock : Solid
     public Sprite sprite;
     public string spriteID;
     private CoreModes coreMode;
+    private bool fireMode;
     public DefrostableBlock(EntityData data, Vector2 offset) : base(data.Position + offset - new Vector2(data.Bool("big")? 16 : 8),
         data.Bool("big", false) ? 32 : 16, data.Bool("big", false) ? 32 : 16, false)
     {
@@ -31,12 +32,13 @@ public class DefrostableBlock : Solid
             sprite.Play("Big");
         else
             sprite.Play("Small");
+        Add(new CoreModeListener(OnChangeMode));
     }
 
     public override void Update()
     {
         Level level = SceneAs<Level>();
-        if (CollideCheck<Shot>() || coreMode == Session.CoreModes.Hot)
+        if (CollideCheck<Shot>() || fireMode)
             defrosting = true;
         if (defrosting)
             Add(new Coroutine(Defrost()));
@@ -57,5 +59,10 @@ public class DefrostableBlock : Solid
             SceneAs<Level>().ParticlesFG.Emit(defrostParticle, 5, Center, Vector2.One * 6f);
         RemoveSelf();
         yield break;
+    }
+
+    public void OnChangeMode(Session.CoreModes coreMode)
+    {
+        fireMode = coreMode == Session.CoreModes.Hot;
     }
 }
