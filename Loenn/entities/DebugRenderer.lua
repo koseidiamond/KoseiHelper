5f8fc6e960550eb337893e021ff6074b637eca93
Kosei <109972200+koseidiamond@users.noreply.github.com>
2025-01-10 19:37:36 +0100

    Initial version of DebugRenderer / hide IRLController and TriggerSpinner

---
+++ Loenn/entities/DebugRenderer.lua
@@ -0,0 +1,111 @@
+local utils = require("utils")
+
+local DebugRenderer = {}
+DebugRenderer.name = "KoseiHelper/DebugRenderer"
+DebugRenderer.depth = -1
+DebugRenderer.nodeLineRenderType = "line"
+DebugRenderer.nodeLimits = {0,1}
+DebugRenderer.nodeVisibility= "always"
+DebugRenderer.fieldInformation = {
+    depth = {
+        fieldType = "integer"
+    },
+    color = {
+        fieldType = "color"
+    },
+    alpha = {
+        minimumValue = 0.0,
+        maximumValue = 1.0
+    }
+}
+DebugRenderer.placements = {
+    name = "DebugRenderer",
+    data = {
+        width = 8,
+        height = 8,
+        color = "000000",
+		shape = "HollowRectangle",
+		flag = "",
+		message = "text",
+		font = "Consolas12",
+		fontSize = 1,
+		ellipseSegments = 9999
+    }
+}
+
+DebugRenderer.fieldOrder = {
+	"x",
+	"y",
+	"width",
+	"height",
+	"color",
+	"shape",
+	"flag",
+	"message",
+	"font",
+	"fontSize",
+	"ellipseSegments"
+}
+
+DebugRenderer.fieldInformation = {
+	shape = {
+		options = {
+			"HollowRectangle",
+			"FilledRectangle",
+			"Circle",
+			"Point",
+			"Line",
+			"Text"
+		},
+		editable = false
+	},
+	font = {
+		options = {
+			"Consolas12",
+			"Renogare"
+		},
+		editable = false
+	},
+	color = { fieldType = "color" },
+	ellipseSegments = {
+		fieldType = "integer",
+		minimumValue = 3
+	}
+}
+
+function DebugRenderer.ignoredFields(entity)
+	local ignored = {
+	"_name",
+    "_id",
+	"font",
+	"fontSize",
+	"message"
+	}
+    local function doNotIgnore(value)
+        for i = #ignored, 1, -1 do
+            if ignored[i] == value then
+                table.remove(ignored, i)
+                return
+            end
+        end
+    end
+	if entity.shape == "Text" then
+		doNotIgnore("font")
+		doNotIgnore("fontSize")
+		doNotIgnore("message")
+	end
+	return ignored
+end
+
+function DebugRenderer.color(room, entity)
+    local color = {0, 0, 0}
+    if entity.color then
+        local success, r, g, b = utils.parseHexColor(entity.color)
+        if success then
+            color = {r, g, b}
+        end
+    end
+    return color
+end
+
+return DebugRenderer
---
+++ Source/Entities/DebugRenderer.cs
@@ -0,0 +1,110 @@
+using Celeste.Mod.Entities;
+using Monocle;
+using Microsoft.Xna.Framework;
+using System.Collections;
+using System;
+
+namespace Celeste.Mod.KoseiHelper.Entities;
+
+[CustomEntity("KoseiHelper/DebugRenderer")]
+public class DebugRenderer : Entity
+{
+    public Color color;
+    public float width, height;
+    public Vector2 node;
+    public string flagName;
+    public bool flagValue;
+    public string message;
+    public int ellipseSegments;
+    public float fontSize;
+    public enum Shape
+    {
+        HollowRectangle,
+        FilledRectangle,
+        Circle,
+        Point,
+        Line,
+        Text
+    }
+    public Shape shape;
+    public enum Font
+    {
+        Consolas12,
+        Renogare
+    }
+    public Font font;
+    public DebugRenderer(EntityData data, Vector2 offset) : base(data.Position + offset)
+    {
+        color = data.HexColor("color", Calc.HexToColor("ffffff"));
+        width = data.Width;
+        height = data.Height;
+        shape = data.Enum("shape", Shape.HollowRectangle);
+        node = data.Nodes.Length > 0 ? data.Nodes[0] + offset : Vector2.Zero;
+        flagName = data.Attr("flag", "");
+        message = data.Attr("message", "text");
+        font = data.Enum("font", Font.Consolas12);
+        fontSize = data.Float("fontSize", 1f);
+        ellipseSegments = data.Int("ellipseSegments", 9999);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (!string.IsNullOrEmpty(flagName))
+            flagValue = SceneAs<Level>().Session.GetFlag(flagName);
+    }
+
+    public override void DebugRender(Camera camera)
+    {
+        if (flagValue || string.IsNullOrEmpty(flagName))
+        {
+            switch (shape)
+            {
+                case Shape.HollowRectangle:
+                    Draw.HollowRect(X, Y, width, height, color);
+                    break;
+                case Shape.FilledRectangle:
+                    Draw.Rect(X, Y, width, height, color);
+                    break;
+                case Shape.Circle:
+                    if (width == height && ellipseSegments > 9998)
+                        Draw.Circle(new Vector2(X + width / 2, Y + height / 2), width / 2, color, 1);
+                    else
+                        DrawEllipse(X + width / 2, Y + height / 2, width / 2, height / 2, color);
+                    break;
+                case Shape.Point:
+                    Draw.Point(this.Position, color);
+                    break;
+                case Shape.Line:
+                    Draw.Line(new Vector2(X, Y), node, color);
+                    break;
+                case Shape.Text:
+                    switch (font)
+                    {
+                        case Font.Consolas12:
+                            Draw.Text(Draw.DefaultFont, message, new Vector2(X, Y), color, Vector2.Zero, Vector2.One * fontSize, 0);
+                            break;
+                        case Font.Renogare:
+                            ActiveFont.Draw(message, new Vector2(X, Y), Vector2.Zero, Vector2.One * fontSize / 2, color);
+                            break;
+                    }
+                    break;
+            }
+            base.DebugRender(camera);
+        }
+    }
+
+    private void DrawEllipse(float x, float y, float rx, float ry, Color color)
+    {
+        Vector2 prevPoint = new Vector2(x + rx, y);
+        for (int i = 1; i <= ellipseSegments; i++)
+        {
+            float theta = MathHelper.TwoPi * i / ellipseSegments;
+            float dx = rx * (float)Math.Cos(theta);
+            float dy = ry * (float)Math.Sin(theta);
+            Vector2 newPoint = new Vector2(x + dx, y + dy);
+            Draw.Line(prevPoint, newPoint, color);
+            prevPoint = newPoint;
+        }
+    }
+}
