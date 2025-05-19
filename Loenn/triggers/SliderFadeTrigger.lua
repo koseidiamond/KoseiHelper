a86cc375eeaeb7d22ab174a477461eac3140c1c3
Kosei <109972200+koseidiamond@users.noreply.github.com>
2025-05-19 12:23:17 +0200

    SliderFadeTrigger done (modifies a slider value)

---
+++ Loenn/triggers/SliderFadeTrigger.lua
@@ -0,0 +1,38 @@
+local SliderFadeTrigger = {}
+local enums = require("consts.celeste_enums")
+
+SliderFadeTrigger.name = "KoseiHelper/SliderFadeTrigger"
+SliderFadeTrigger.placements = {
+    name = "SliderFadeTrigger",
+    data = {
+		sliderName = "",
+		onlyOnce = false,
+		fadeFrom = 0,
+		fadeTo = 0.5,
+		positionMode = "NoEffect"
+    }
+}
+
+SliderFadeTrigger.fieldInformation = 
+{
+	positionMode = {
+        options = enums.trigger_position_modes,
+        editable = false
+    }
+}
+
+SliderFadeTrigger.fieldOrder = 
+{
+	"x",
+	"y",
+	"width",
+	"height",
+	"positionMode",
+	"widthFrom",
+	"widthTo",
+	"heightFrom",
+	"heightTo",
+	"fullScreen"
+}
+
+return SliderFadeTrigger
---
+++ Source/Triggers/SliderFadeTrigger.cs
@@ -0,0 +1,40 @@
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Celeste.Mod.KoseiHelper.Triggers;
+
+[CustomEntity("KoseiHelper/SliderFadeTrigger")]
+public class SliderFadeTrigger : Trigger
+{
+    public float fadeFrom, fadeTo;
+    private PositionModes positionMode;
+    public bool onlyOnce;
+    public string direction;
+    public string sliderName;
+    public SliderFadeTrigger(EntityData data, Vector2 offset) : base(data, offset)
+    {
+        sliderName = data.Attr("sliderName", "");
+        onlyOnce = data.Bool("fullScreen", false);
+        direction = data.Attr("positionMode");
+        fadeFrom = data.Float("fadeFrom", 0f);
+        fadeTo = data.Float("fadeTo", 0.5f);
+        if (!string.IsNullOrEmpty(direction) && Enum.TryParse<PositionModes>(direction.ToString(), ignoreCase: true, out var result))
+            positionMode = result;
+    }
+
+    public override void OnStay(Player player)
+    {
+        base.OnStay(player);
+        Level level = SceneAs<Level>();
+        float positionLerp = GetPositionLerp(player, positionMode);
+        level.Session.SetSlider(sliderName, MathHelper.Lerp(fadeFrom, fadeTo, positionLerp));
+    }
+
+    public override void OnLeave(Player player)
+    {
+        base.OnLeave(player);
+        if (onlyOnce)
+            RemoveSelf();
+    }
+}
