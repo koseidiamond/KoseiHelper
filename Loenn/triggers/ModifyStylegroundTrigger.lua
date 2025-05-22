f68a97b41d7c0a5b3c4e543377f0c6b83d806ab5
Kosei <109972200+koseidiamond@users.noreply.github.com>
2025-05-22 22:04:25 +0200

    styleground modifier alpha version

---
+++ Loenn/triggers/ModifyStylegroundTrigger.lua
@@ -0,0 +1,219 @@
+local ModifyStylegroundTrigger = {}
+
+ModifyStylegroundTrigger.name = "KoseiHelper/ModifyStylegroundTrigger"
+ModifyStylegroundTrigger.depth = 100
+
+ModifyStylegroundTrigger.placements = {
+	{
+		name = "ModifyStylegroundTrigger",
+		alternativeName = "StylegroundModifierTrigger",
+		data = {
+		onlyOnce = false,
+		triggerMode = "OnEnter",
+		slider = "",
+		sliderMultiplier = 1,
+		
+		identificationMode = "Index",
+		index = 0,
+		tag = "",
+		texture = "",
+		
+		fieldToModify = "Color",
+		flag = "",
+		notFlag = "",
+		color = "FFFFFF",
+		positionX = 0,
+		positionY = 0,
+		scrollX = 0,
+		scrollY = 0,
+		speedX = 0,
+		speedY = 0,
+		alpha = 1,
+		fadeIn = false,
+		flipX = false,
+		flipY = false,
+		instantIn = false,
+		instantOut = false,
+		loopX = false,
+		loopY = false
+		}
+	}
+}
+
+ModifyStylegroundTrigger.fieldInformation = function (entity) return {
+	triggerMode = {
+		options = {
+		"OnEnter",
+		"OnLeave",
+		"OnStay"
+		},
+		editable = false
+	},
+	identificationMode = {
+		options = {
+		"Index",
+		"Tag",
+		"Texture"
+		},
+		editable = false
+	},
+	fieldToModify = {
+		options = {
+		"Flag",
+		"NotFlag",
+		"Color",
+		"PositionX",
+		"PositionY",
+		"ScrollX",
+		"ScrollY",
+		"SpeedX",
+		"SpeedY",
+		"Alpha",
+		"FadeIn",
+		"FlipX",
+		"FlipY",
+		"InstantIn",
+		"InstantOut",
+		"LoopX",
+		"LoopY"
+		},
+		editable = false
+	},
+	index = {
+		fieldType = "integer",
+		minimumValue = 0
+	},
+	alpha = {
+		fieldType = "number",
+		minimumValue = 0
+	},
+	color = {
+		fieldType = "color"
+	}
+}
+end
+
+ModifyStylegroundTrigger.fieldOrder= {
+	"x",
+	"y",
+	"width",
+	"height",
+	"identificationMode",
+	"index",
+	"tag",
+	"texture",
+	"fieldToModify",
+	"triggerMode",
+	"slider",
+	"sliderMultiplier"
+}
+
+function ModifyStylegroundTrigger.ignoredFields(entity)
+	local ignored = {
+	"_name",
+	"_id",
+	"slider",
+	"sliderMultiplier",
+	
+	"index",
+	"tag",
+	"texture",
+	
+	"flag",
+	"notFlag",
+	"color",
+	"positionX",
+	"positionY",
+	"scrollX",
+	"scrollY",
+	"speedX",
+	"speedY",
+	"alpha",
+	"fadeIn",
+	"flipX",
+	"flipY",
+	"instantIn",
+	"instantOut",
+	"loopX",
+	"loopY"
+	}
+    local function doNotIgnore(value)
+        for i = #ignored, 1, -1 do
+            if ignored[i] == value then
+                table.remove(ignored, i)
+                return
+            end
+        end
+    end
+
+	if entity.identificationMode == "Index" then
+		doNotIgnore("index")
+	end
+	if entity.identificationMode == "Tag" then
+		doNotIgnore("tag")
+	end
+	if entity.identificationMode == "Texture" then
+		doNotIgnore("texture")
+	end
+	
+	if entity.fieldToModify == "Flag" then
+		doNotIgnore("flag")
+	end
+	if entity.fieldToModify == "NotFlag" then
+		doNotIgnore("notFlag")
+	end
+	if entity.fieldToModify == "Color" then
+		doNotIgnore("color")
+	end
+	if entity.fieldToModify == "PositionX" then
+		doNotIgnore("positionX")
+	end
+	if entity.fieldToModify == "PositionY" then
+		doNotIgnore("positionY")
+	end
+	if entity.fieldToModify == "ScrollX" then
+		doNotIgnore("scrollX")
+	end
+	if entity.fieldToModify == "ScrollY" then
+		doNotIgnore("scrollY")
+	end
+	if entity.fieldToModify == "SpeedX" then
+		doNotIgnore("speedX")
+	end
+	if entity.fieldToModify == "SpeedY" then
+		doNotIgnore("speedY")
+	end
+	if entity.fieldToModify == "Alpha" then
+		doNotIgnore("alpha")
+	end
+	if entity.fieldToModify == "FadeIn" then
+		doNotIgnore("fadeIn")
+	end
+	if entity.fieldToModify == "FlipX" then
+		doNotIgnore("flipX")
+	end
+	if entity.fieldToModify == "FlipY" then
+		doNotIgnore("flipY")
+	end
+	if entity.fieldToModify == "InstantIn" then
+		doNotIgnore("instantIn")
+	end
+	if entity.fieldToModify == "InstantOut" then
+		doNotIgnore("instantOut")
+	end
+	if entity.fieldToModify == "LoopX" then
+		doNotIgnore("loopX")
+	end
+	if entity.fieldToModify == "LoopY" then
+		doNotIgnore("loopY")
+	end
+	if entity.fieldToModify == "PositionX" or entity.fieldToModify == "PositionY" or entity.fieldToModify == "ScrollX" or entity.fieldToModify == "ScrollY"
+	or entity.fieldToModify == "SpeedX" or entity.fieldToModify == "SpeedY" or entity.fieldToModify == "Alpha" then
+		doNotIgnore("slider")
+		doNotIgnore("sliderMultiplier")
+	end
+	
+	return ignored
+end
+
+return ModifyStylegroundTrigger
---
+++ Source/Triggers/ModifyStylegroundTrigger.cs
@@ -0,0 +1,218 @@
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.KoseiHelper.Triggers;
+
+
+[CustomEntity("KoseiHelper/ModifyStylegroundTrigger")]
+public class ModifyStylegroundTrigger : Trigger
+{
+    public bool onlyOnce;
+    public TriggerMode triggerMode;
+    public string slider;
+    public float sliderFinal;
+    public float sliderMultiplier;
+
+    public Backdrop backdrop;
+
+    public enum IdentificationMode
+    {
+        Index,
+        Tag,
+        Texture
+    };
+    public IdentificationMode identificationMode;
+    public int index;
+    public string tag;
+    public string texture;
+
+    public enum FieldToModify
+    {
+        Flag,
+        NotFlag,
+        Color,
+        PositionX,
+        PositionY,
+        ScrollX,
+        ScrollY,
+        SpeedX,
+        SpeedY,
+        Alpha,
+        FadeIn,
+        FlipX,
+        FlipY,
+        InstantIn,
+        InstantOut,
+        LoopX,
+        LoopY
+    };
+    public FieldToModify fieldToModify;
+    public string flag, notFlag;
+    public Color color;
+    public float positionX, positionY;
+    public float scrollX, scrollY;
+    public float speedX, speedY;
+    public float alpha;
+    public bool fadeIn;
+    public bool flipX, flipY;
+    public bool instantIn, instantOut;
+    public bool loopX, loopY;
+
+    public ModifyStylegroundTrigger(EntityData data, Vector2 offset) : base(data, offset)
+    {
+        onlyOnce = data.Bool("onlyOnce", false);
+        triggerMode = data.Enum("triggerMode", TriggerMode.OnEnter);
+        slider = data.Attr("slider", "");
+        sliderMultiplier = data.Float("sliderMultiplier", 1f);
+
+        identificationMode = data.Enum("identificationMode", IdentificationMode.Index);
+        index = data.Int("index", 0);
+        tag = data.Attr("tag", "");
+        texture = data.Attr("texture", "");
+
+        fieldToModify = data.Enum("fieldToModify", FieldToModify.Color);
+        flag = data.Attr("flag", "");
+        notFlag = data.Attr("notFlag", "");
+        color = data.HexColor("color", Color.White);
+        positionX = data.Float("positionX", 0f);
+        positionY = data.Float("positionY", 0f);
+        scrollX = data.Float("scrollX", 0f);
+        scrollY = data.Float("scrollY", 0f);
+        speedX = data.Float("speedX", 0f);
+        speedY = data.Float("speedY", 0f);
+        alpha = data.Float("alpha", 1f);
+        fadeIn = data.Bool("fadeIn", false);
+        flipX = data.Bool("flipX", false);
+        flipY = data.Bool("flipY", false);
+        instantIn = data.Bool("instantIn", false);
+        instantOut = data.Bool("instantOut", false);
+        loopX = data.Bool("loopX", false);
+        loopY = data.Bool("loopY", false);
+    }
+    public override void OnEnter(Player player)
+    {
+        base.OnEnter(player);
+        if (triggerMode == TriggerMode.OnEnter)
+            FindBackdrop();
+        Remove();
+    }
+
+    public override void OnLeave(Player player)
+    {
+        base.OnLeave(player);
+        if (triggerMode == TriggerMode.OnLeave)
+            FindBackdrop();
+        Remove();
+    }
+
+    public override void OnStay(Player player)
+    {
+        base.OnStay(player);
+        if (triggerMode == TriggerMode.OnStay)
+            FindBackdrop();
+        Remove();
+    }
+
+    private void FindBackdrop()
+    {
+        Level level = SceneAs<Level>();
+        // Example: SceneAs<Level>().Background.Backdrops[index].Color = Color.Red;
+        switch (identificationMode)
+        {
+            case IdentificationMode.Texture:
+                throw new System.Exception("The Texture feature is not implemented yet!");
+                break;
+            case IdentificationMode.Tag:
+                throw new System.Exception("The Tag feature is not implemented yet!");
+                break;
+            default: // Index
+                backdrop = level.Background.Backdrops[index];
+                ModifyBackdrop(backdrop);
+                break;
+        }
+    }
+
+    private void ModifyBackdrop(Backdrop backdrop)
+    {
+        Session session = SceneAs<Level>().Session;
+        if (!string.IsNullOrEmpty(slider))
+            sliderFinal = session.GetSlider(slider) * sliderMultiplier;
+
+        switch (fieldToModify)
+        {
+            case FieldToModify.Flag:
+                backdrop.OnlyIfFlag = flag;
+                break;
+            case FieldToModify.NotFlag:
+                backdrop.OnlyIfNotFlag = notFlag;
+                break;
+            case FieldToModify.PositionX:
+                if (string.IsNullOrEmpty(slider))
+                    backdrop.Position.X = positionX;
+                else
+                    backdrop.Position.X = sliderFinal;
+                    break;
+            case FieldToModify.PositionY:
+                if (string.IsNullOrEmpty(slider))
+                    backdrop.Position.Y = positionY;
+                else
+                    backdrop.Position.Y = sliderFinal;
+                break;
+            case FieldToModify.ScrollX:
+                if (string.IsNullOrEmpty(slider))
+                    backdrop.Scroll.X = scrollX;
+                else
+                    backdrop.Scroll.X = sliderFinal;
+                    break;
+            case FieldToModify.ScrollY:
+                if (string.IsNullOrEmpty(slider))
+                    backdrop.Scroll.Y = scrollY;
+                else
+                    backdrop.Scroll.Y = sliderFinal;
+                    break;
+            case FieldToModify.SpeedX:
+                if (string.IsNullOrEmpty(slider))
+                    backdrop.Speed.X = speedX;
+                else
+                    backdrop.Speed.X = sliderFinal;
+                    break;
+            case FieldToModify.Alpha:
+                if (string.IsNullOrEmpty(slider))
+                    backdrop.FadeAlphaMultiplier = alpha;
+                else
+                    backdrop.FadeAlphaMultiplier = sliderFinal;
+                    break;
+            case FieldToModify.FadeIn:
+                throw new System.Exception("The FadeIn feature is not implemented yet!");
+                break;
+            case FieldToModify.FlipX:
+                backdrop.FlipX = flipX;
+                break;
+            case FieldToModify.FlipY:
+                backdrop.FlipY = flipY;
+                break;
+            case FieldToModify.InstantIn:
+                backdrop.InstantIn = instantIn;
+                break;
+            case FieldToModify.InstantOut:
+                backdrop.InstantOut = instantOut;
+                break;
+            case FieldToModify.LoopX:
+                backdrop.LoopX = loopX;
+                break;
+            case FieldToModify.LoopY:
+                backdrop.LoopY = loopY;
+                break;
+            default: // Color
+                backdrop.Color = color;
+                break;
+        }
+    }
+
+    private void Remove()
+    {
+        if (onlyOnce)
+            RemoveSelf();
+    }
+}
